cb3e777dbdb65fefbba4091fd7520772bd6433f1
Adam Larat <adam@larat.fr>
2022-05-25 15:06:27 +0200

    Execution de run.sh par un utilisateur quelconque. Attention qu'il ait les bons droits en lecture/écriture !

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ## Récupération du nom d'utilisateur pour effectuer les tâches
-if [ "$(whoami)" !=  "root" ]
+if [ $# -ne 1 ]
 then
-   echo "Ce script doit être lancé comme root."
-   echo "Les fichiers sont automatiquement redonné à l'utilisateur www-data à la fin." 
+   echo "Ce script requiert un argument. Vous devez fournir l'utilisateur qui effectuera les tâches !"
+   echo "Usage = ./run.sh user"
    echo "À bientôt !"
    exit 255
 else
+  user = $1
   ## Gestion de la date et de la saison en cours
   today=$(date '+%Y%m%d-%H%M%S')
   mois=$(date '+%m')
@@ -30,17 +31,17 @@ else
   fichierHelloAsso=${dossierLogs}/HelloAsso_${today}.csv
   
   ## Sauvegarde du fichier courant 
-  mkdir -p Logs/$today
-  cp $fichierCourant $fichierBackup
-  libreoffice --convert-to csv:"Text - txt - csv (StarCalc)":59,34,76 --outdir ${dossierAdhesions}/${saison} ${fichierCourant}
+  sudo -u $user mkdir -p Logs/$today
+  sudo -u $user cp $fichierCourant $fichierBackup
+  sudo -u $user libreoffice --convert-to csv:"Text - txt - csv (StarCalc)":59,34,76 --outdir ${dossierAdhesions}/${saison} ${fichierCourant}
   
   ## Téléchargement du fichier HelloAsso
-  wget -q --load-cookies cookies-helloasso-com.txt "https://www.helloasso.com/admin/handler/reports.ashx?type=Details&from=${debut}&includeSubpages=1&period=MONTH&domain=HelloAsso&trans=Adhesions&format=CSV&orgSlug=pic-col&formSlug=adhesion-saison-${saison}&formType=Membership" -O ${fichierHelloAsso}
+  sudo -u $user wget -q --load-cookies cookies-helloasso-com.txt "https://www.helloasso.com/admin/handler/reports.ashx?type=Details&from=${debut}&includeSubpages=1&period=MONTH&domain=HelloAsso&trans=Adhesions&format=CSV&orgSlug=pic-col&formSlug=adhesion-saison-${saison}&formType=Membership" -O ${fichierHelloAsso}
   
   ## Traitement des adhésions par python3 (>= 3.8 pour pylocalc)
-  python3 adhesionsPicEtCol.py ${fichierHelloAsso} ${fichierCourant} | tee ${dossierLogs}/${today}_nouvellesAdhesions.log 
+  sudo -u $user sudo -u $user python3 adhesionsPicEtCol.py ${fichierHelloAsso} ${fichierCourant} | tee ${dossierLogs}/${today}_nouvellesAdhesions.log 
   ## Mise-à-jour du CSV
-  libreoffice --convert-to csv:"Text - txt - csv (StarCalc)":59,34,76 --outdir ${dossierAdhesions}/${saison} ${fichierCourant}
+  sudo -u $user libreoffice --convert-to csv:"Text - txt - csv (StarCalc)":59,34,76 --outdir ${dossierAdhesions}/${saison} ${fichierCourant}
   
   ## Rescan des fichiers par Nextcloud quand on est sur le serveur
   chown www-data:www-data -R ${fichierCSV} ${fichierCourant} ${dossierLogs}
